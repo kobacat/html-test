8ff4a9344685dcfe8b4cbda2f048ef4163cc2a16
Sir Felix <jaspycatty@gmail.com>
2016-09-26 23:04:44 -0400

    Moved Neocities website contents to GitHub.

---
+++ video.js
@@ -0,0 +1,7 @@
+document.getElementById("video1").addEventListener("click", function( event ) {
+        document.getElementById("vidya")
+      }, false);
+      
+document.getElementById("video2").addEventListener("click", function( event ) {
+        document.getElementById("vidya")
+      }, false);
